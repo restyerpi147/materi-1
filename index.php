21fa27942cd9cc4021795f5b858f278c0b97cd75
restyerpi147 <71168506+restyerpi147@users.noreply.github.com>
2020-09-13 16:25:22 +0700

    belajar instance object

--- index.php
+++ index.php
@@ -12,11 +12,24 @@ class Mobil {
 	}
 }
 	
-
 $bmw = new mobil;
 $bmw->merk ="BMW";
 $bmw->tipe ="320i";
 $bmw->mesin ="2000cc";
 $bmw->max_speed ="280km/h";
 
-echo "Tipe ".$bmw->kecepatanMaksimal();
+$bmw2 = new mobil;
+$bmw2->merk ="BMW";
+$bmw2->tipe ="320i";
+$bmw2->mesin ="2000cc";
+$bmw2->max_speed ="280km/h";
+
+echo $bmw2->tipe;
+echo "<br>";
+echo $bmw->tipe;
+
+if($bmw === $bmw2){
+	echo "Sama";
+}else{
+	echo "Tidak";
+}
