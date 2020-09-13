5700d096f0e73c80685721e22b536ae793f373dc
restyerpi147 <71168506+restyerpi147@users.noreply.github.com>
2020-09-13 17:37:36 +0700

    belajar polymorphism

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 class Mobil {
-	private $merk, $tipe, $mesin, $max_speed;
+	public $merk, $tipe, $mesin, $max_speed;
 
 	public function cetakTipe(){
 		return $this->tipe;
@@ -10,6 +10,11 @@ class Mobil {
 	 public function kecepatanMaksimal(){
 		return "Kecepatan Maksimal Dari Mobil Ini Adalah ".$this->max_speed;
 	}
+
+	function injekGas(){
+		return "mengalirkan bensin ke ruang bakar,rpm naik, roda berputar ";
+	}
+
 }
 	
 class BMW extends Mobil {
@@ -19,8 +24,12 @@ class BMW extends Mobil {
 class Tesla extends Mobil {
 	function selfParking(){
 		echo "Parkir Sendiri";
-
 	}
+
+	function injekGas(){
+		return "mengalirkan listrik ke dinamo, rpm naik, roda berputar";
+	}
+
 }
 
 $bmw = new BMW;
@@ -37,4 +46,7 @@ $tesla->max_speed ="280km/h";
 
 
 
-echo $tesla->merk;
+echo "BMW : ".$bmw->injekGas();
+echo "<br>";
+echo "<br>";
+echo "Tesla : ".$tesla->injekGas();
